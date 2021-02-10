7b27b68b9a1ba44d24f6e12febd115ecda3fda53
Szymon Dąbrowski <szymonmiloszdabrowski@gmail.com>
2021-02-10 19:55:35 +0100

    hotfix

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <h2>Sukces</h2>
         <h3>jest jak drabina</h3>
         <h4>nie wejdziesz na nią trzymając ręce w kieszeni</h4>
-        <Link to={'/lgoin'}>
+        <Link to={'/login'}>
           <img src="images/arrowButton.svg" alt="Sign up button" />
         </Link>
       </div>
